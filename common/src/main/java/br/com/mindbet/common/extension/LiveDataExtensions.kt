a630e6cf37ceab5f980ffe87033cd4a105f868ff
Da Silva, Luan Gabriel <Luan.Silva@gft.com>
2020-06-28 21:38:09 -0300

    Update LiveDataExtensions.kt
    single live event ext

--- common/src/main/java/br/com/mindbet/common/extension/LiveDataExtensions.kt
+++ common/src/main/java/br/com/mindbet/common/extension/LiveDataExtensions.kt
@@ -3,7 +3,6 @@ package br.com.mindbet.common.extension
 import androidx.lifecycle.LiveData
 import br.com.mindbet.common.helper.SingleLiveEvent
 
-
 fun <T> LiveData<T>.toSingleEvent(): LiveData<T> {
     val result = SingleLiveEvent<T>()
     result.addSource(this) {
